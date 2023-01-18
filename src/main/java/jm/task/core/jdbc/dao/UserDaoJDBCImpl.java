8ef4ab11da8895b79fdde3e5c0625c4a8c3b48a3
MalkovAlex <amalkov681@gmail.com>
2023-01-19 03:40:19 +0500

    with connection & create table

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,15 +1,33 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+
+        String sql = "CREATE TABLE IF NOT EXIST`myDbtest`.`users` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                "  `age` INT(3) NOT NULL,\n" +
+                "  PRIMARY KEY (`id`));";
+        try (Statement statement = connection.createStatement()){
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,19 +6,21 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/myDbtest";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "rootroot";
 
     public static Connection getConnection () {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)){
+            Class.forName(DB_DRIVER);
             System.out.println("Connection OK");
-        } catch (SQLException e) {
-            System.err.println("Connection ERROR");;
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Connection ERROR");
+            return null;
         }
-        return connection;
     }
 
 
